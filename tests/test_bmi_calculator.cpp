771c9e2ddf828cd6c956d399738295355d7c7bbe
bjproxy <112009871+bjproxy@users.noreply.github.com>
2023-04-13 23:00:15 -0500

    Create test_bmi_calculator.cpp

---
+++ tests/test_bmi_calculator.cpp
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+#include "../src/bmi_calculator.h"
+
+TEST(BMICalculatorTest, Underweight) {
+    EXPECT_EQ(calculate_bmi_category(16.0), "You are underweight.");
+}
+
+TEST(BMICalculatorTest, NormalWeight) {
+    EXPECT_EQ(calculate_bmi_category(21.0), "You have a normal weight.");
+}
+
+TEST(BMICalculatorTest, Overweight) {
+    EXPECT_EQ(calculate_bmi_category(27.0), "You are overweight.");
+}
+
+TEST(BMICalculatorTest, Obese) {
+    EXPECT_EQ(calculate_bmi_category(32.0), "You are obese.");
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
