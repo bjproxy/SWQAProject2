d50f8c1e4f7ee1b17e0c2d2cf64d7c81e13481e9
bjproxy <112009871+bjproxy@users.noreply.github.com>
2023-04-13 23:41:30 -0500

    Update test_bmi_calculator.cpp

--- tests/test_bmi_calculator.cpp
+++ tests/test_bmi_calculator.cpp
@@ -1,23 +1,54 @@
 #include <gtest/gtest.h>
-#include "../src/bmi_calculator.cpp"
+#include <cmath>
 
-TEST(BMICalculatorTest, Underweight) {
-    EXPECT_EQ(calculate_bmi_category(16.0), "You are underweight.");
+#include "bmi_calculator.cpp"
+
+TEST(BMICalculator, Underweight)
+{
+    // Test for underweight
+    float weight = 100;
+    float height = 72;
+    float bmi = calculate_bmi(weight, height);
+    float expected_bmi = roundf((weight / (height * height)) * 703 * 10) / 10;
+    EXPECT_EQ(expected_bmi, bmi);
+    EXPECT_EQ("underweight", get_bmi_category(bmi));
 }
 
-TEST(BMICalculatorTest, NormalWeight) {
-    EXPECT_EQ(calculate_bmi_category(21.0), "You have a normal weight.");
+TEST(BMICalculator, NormalWeight)
+{
+    // Test for normal weight
+    float weight = 150;
+    float height = 68;
+    float bmi = calculate_bmi(weight, height);
+    float expected_bmi = roundf((weight / (height * height)) * 703 * 10) / 10;
+    EXPECT_EQ(expected_bmi, bmi);
+    EXPECT_EQ("normal weight", get_bmi_category(bmi));
 }
 
-TEST(BMICalculatorTest, Overweight) {
-    EXPECT_EQ(calculate_bmi_category(27.0), "You are overweight.");
+TEST(BMICalculator, Overweight)
+{
+    // Test for overweight
+    float weight = 180;
+    float height = 70;
+    float bmi = calculate_bmi(weight, height);
+    float expected_bmi = roundf((weight / (height * height)) * 703 * 10) / 10;
+    EXPECT_EQ(expected_bmi, bmi);
+    EXPECT_EQ("overweight", get_bmi_category(bmi));
 }
 
-TEST(BMICalculatorTest, Obese) {
-    EXPECT_EQ(calculate_bmi_category(32.0), "You are obese.");
+TEST(BMICalculator, Obese)
+{
+    // Test for obese
+    float weight = 220;
+    float height = 68;
+    float bmi = calculate_bmi(weight, height);
+    float expected_bmi = roundf((weight / (height * height)) * 703 * 10) / 10;
+    EXPECT_EQ(expected_bmi, bmi);
+    EXPECT_EQ("obese", get_bmi_category(bmi));
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv)
+{
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
